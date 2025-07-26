2257134cfebc3c47a9627c86b37415d7d51088cc
gitApurv <apurvmaurya5@gmail.com>
2025-07-26 13:21:16 +0530

    refactor: Remove unused footer links for cleaner design

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -11,16 +11,6 @@ const Footer = () => {
         <div className="mb-4 md:mb-0">
           <Image src={Logo} alt="Logo" className="h-8 w-auto" />
         </div>
-        <div className="flex flex-wrap justify-center md:justify-start mb-4 md:mb-0">
-          <ul className="flex space-x-4">
-            <li>
-              <Link href="/properties">Properties</Link>
-            </li>
-            <li>
-              <Link href="/terms">Terms of Service</Link>
-            </li>
-          </ul>
-        </div>
         <div>
           <p className="text-sm text-gray-500 mt-2 md:mt-0">
             &copy; {currentYear} NextStay. All rights reserved.
